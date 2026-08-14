01a3eb2bdac6e41b207dd5db300345c9e2e9cd8a
Jayprakash <0freerunning@gmail.com>
2026-08-14 19:12:44 +0530

    Removed the env file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue(), vuetify({ autoImport: true })],
-})
+  base: command === "build" ? "/static/" : "/",
+}));
